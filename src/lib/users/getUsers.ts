a2ca2ad72f578477e42dc210fd13e5a0a49e2dcd
Leonardo Crispim <leonardocrispimweb@gmail.com>
2023-06-20 12:43:04 -0300

    👌 MELHORIA: correção login

--- src/lib/users/getUsers.ts
+++ src/lib/users/getUsers.ts
@@ -2,6 +2,7 @@ import { URL_BACKEND } from '@utils/utils';
 
 export async function getUserLogin(username: string, password: string) {
   const response = await fetch(`${URL_BACKEND}/users/login`, {
+    cache: 'no-cache',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
